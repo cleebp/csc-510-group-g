df765bf2f774c59ba9e932769031ef7de5905357
Arjun Madan <arjunmadan1992@gmail.com>
2016-03-23 22:49:46 -0400

    Fixed windows clipboard

--- mar1/telem/telem-windows.py
+++ mar1/telem/telem-windows.py
@@ -1,23 +1,36 @@
 from time import gmtime, strftime
 import sys, getopt
-import win32clipboard
 import win32gui
+import ctypes
+
+ocb = ctypes.windll.user32.OpenClipboard    #Basic Clipboard functions
+ecb = ctypes.windll.user32.EmptyClipboard
+gcd = ctypes.windll.user32.GetClipboardData
+scd = ctypes.windll.user32.SetClipboardData
+ccb = ctypes.windll.user32.CloseClipboard
+ga = ctypes.windll.kernel32.GlobalAlloc    # Global Memory allocation
+gl = ctypes.windll.kernel32.GlobalLock     # Global Memory Locking
+gul = ctypes.windll.kernel32.GlobalUnlock
+GMEM_DDESHARE = 0x2000 
+
+def Get():
+	ocb(None)
+	pcontents = gcd(1) 
+	data = ctypes.c_char_p(pcontents).value
+	ccb()
+	return data
 
 def startTelem(outfile):
+	prev = Get()
 	
-	prev = ""
-
 	while True:
-		# get clipboard data
-		win32clipboard.OpenClipboard()
-		clip_text = win32clipboard.GetClipboardData()
-		win32clipboard.CloseClipboard()
-		
-		if clip_text != prev:
+		clip_text = Get()
+
+		if clip_text != prev and clip_text != None:
 			prev = clip_text
 			window = get_active_window_title()
 			with open(outfile, "a+") as myfile:
-				myfile.write(str(clip_text) + ', ' +  str(strftime("%Y-%m-%d %H:%M:%S")) + ', ' + str(window) + '\n')
+				myfile.write(str(clip_text) + ', ' + str(strftime("%Y-%m-%d %H:%M:%S")) + ', ' + str(window) + '\n')
 
 def get_active_window_title():
 	w = win32gui
@@ -26,6 +39,7 @@ def get_active_window_title():
 
 def main(argv):
 	outputfile = ''
+
 	try:
 		opts, args = getopt.getopt(argv,"ho:",["ofile="])
 	except getopt.GetoptError:
@@ -33,13 +47,11 @@ def main(argv):
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt == '-h':
- 			print 'telem-windos.py -o <outputfilename> (default path is current directory)'
- 			sys.exit()
+			print 'telem-windos.py -o <outputfilename> (default path is current directory)'
+			sys.exit()
 		elif opt in ("-o", "--ofile"):
 			outputfile = "./" + arg
-	startTelem(outputfile)
-	
+			startTelem(outputfile)
 
-   
 if __name__ == "__main__":
-   main(sys.argv[1:])
+	main(sys.argv[1:])
