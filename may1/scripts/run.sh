4d7462a715ee696daa34b4b740a5338c31d70f5a
Arjun Madan <arjunmadan1992@gmail.com>
2016-04-25 00:10:45 -0400

    Update dbs to include additions, deletions

--- may1/scripts/run.sh
+++ may1/scripts/run.sh
@@ -1,8 +1,8 @@
-for repo in "group-a" "group-b" "group-c" "group-d" "group-e" "group-f" "group-g" "group-h" "group-i" "group-j" "group-k" "group-l" "group-m" "group-n"
-do sqlite3 -csv ${repo}.db "select * from milestone;" > ./dmp/${repo}_milestone.csv
-sqlite3 -csv ${repo}.db "select * from issue;" > ./dmp/${repo}_issue.csv
-sqlite3 -csv ${repo}.db "select * from event;" > ./dmp/${repo}_event.csv
-sqlite3 -csv ${repo}.db "select * from comment;" > ./dmp/${repo}_comment.csv
-sqlite3 -csv ${repo}.db "select * from commits;" > ./dmp/${repo}_commits.csv
+for repo in "group-b" "group-c" "group-f" "group-g" "group-i" "group-m"
+do sqlite3 -csv ../data/databases/${repo}.db "select * from milestone;" > ../data/dmp/${repo}_milestone.csv
+sqlite3 -csv ../data/databases/${repo}.db "select * from issue;" > ../data/dmp/${repo}_issue.csv
+sqlite3 -csv ../data/databases/${repo}.db "select * from event;" > ../data/dmp/${repo}_event.csv
+sqlite3 -csv ../data/databases/${repo}.db "select * from comment;" > ../data/dmp/${repo}_comment.csv
+sqlite3 -csv ../data/databases/${repo}.db "select * from commits;" > ../data/dmp/${repo}_commits.csv
 done
-find ./dmp/ -size  0 -exec rm '{}' \;
+find ../data/dmp/ -size  0 -exec rm '{}' \;
